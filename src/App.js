24ccfcd3892fb2fd68f2eb0d56addfbacc48797f
Triston-254 <abelnyamora@gmail.com>
2026-09-26 15:04:44 +0300

    Show clear report action after generation

--- src/App.js
+++ src/App.js
@@ -2125,9 +2125,11 @@ function App() {
                 <p>Reports</p>
                 <h2>Generate and download reports</h2>
               </div>
-              <button onClick={clearReport} className="cancel-edit">
-                <Icon name="close" /> Clear Report
-              </button>
+              {generatedReport && (
+                <button type="button" onClick={clearReport} className="cancel-edit">
+                  <Icon name="close" /> Clear Report
+                </button>
+              )}
             </div>
             
             <div className="report-generator">
